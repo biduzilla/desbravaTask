250fbe81f94e968df69dfa75c7239df8f2129a95
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-02-23 22:51:04 -0300

    projeto vazio

---
+++ api/src/test/kotlin/com/ricky/desbravaTask/DesbravaTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ricky.desbravaTask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesbravaTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
