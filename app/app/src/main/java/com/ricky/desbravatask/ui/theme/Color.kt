13f8073542488b1979f67165fcc048e4a23e06cd
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-03-07 08:15:02 -0300

    color theme

--- app/app/src/main/java/com/ricky/desbravatask/ui/theme/Color.kt
+++ app/app/src/main/java/com/ricky/desbravatask/ui/theme/Color.kt
@@ -9,3 +9,60 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryLight = Color(0xFF009688)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFFF5252)
+val OnPrimaryContainerLight = Color(0xFF212121)
+
+val SecondaryLight = Color(0xFF535f70)
+val OnSecondaryLight = Color(0xFFBDBDBD)
+val SecondaryContainerLight = Color(0xFFd7e3f7)
+val OnSecondaryContainerLight = Color(0xFF757575)
+
+val TertiaryLight = Color(0xFF6b5778)
+val OnTertiaryLight = Color(0xFFffffff)
+val TertiaryContainerLight = Color(0xFFf2daff)
+val OnTertiaryContainerLight = Color(0xFF251431)
+
+val ErrorLight = Color(0xFFba1a1a)
+val OnErrorLight = Color(0xFFffffff)
+val ErrorContainerLight = Color(0xFFffdad6)
+val OnErrorContainerLight = Color(0xFF410002)
+
+val BackgroundLight = Color(0xFFfdfcff)
+val OnBackgroundLight = Color(0xFF1a1c1e)
+val SurfaceLight = Color(0xFFfdfcff)
+
+val OutlineLight = Color(0xFF73777f)
+val SurfaceVariantLight = Color(0xFFdfe2eb)
+val OnSurfaceVariantLight = Color(0xFF43474e)
+
+// Dark
+val PrimaryDark = Color(0xFF00796B)
+val OnPrimaryDark = Color(0xFFFFFFFF)
+val PrimaryContainerDark = Color(0xFFFF5252)
+val OnPrimaryContainerDark = Color(0xFF212121)
+
+val SecondaryDark = Color(0xFFbbc7db)
+val OnSecondaryDark = Color(0xFF253140)
+val SecondaryContainerDark = Color(0xFF3b4858)
+val OnSecondaryContainerDark = Color(0xFFd7e3f7)
+
+val TertiaryDark = Color(0xFFd7bee4)
+val OnTertiaryDark = Color(0xFF3b2948)
+val TertiaryContainerDark = Color(0xFF523f5f)
+val OnTertiaryContainerDark = Color(0xFFf2daff)
+
+val ErrorDark = Color(0xFFffb4ab)
+val OnErrorDark = Color(0xFF690005)
+val ErrorContainerDark = Color(0xFF93000a)
+val OnErrorContainerDark = Color(0xFFffdad6)
+
+val BackgroundDark = Color(0xFF1a1c1e)
+val OnBackgroundDark = Color(0xFFe2e2e6)
+val SurfaceDark = Color(0xFF1a1c1e)
+
+val OutlineDark = Color(0xFF8d9199)
+val SurfaceVariantDark = Color(0xFF43474e)
+val OnSurfaceVariantDark = Color(0xFFc3c7cf)
