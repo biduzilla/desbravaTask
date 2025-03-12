b6183c7300539cf1e79756d696b1f17add0f98ed
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-03-12 14:39:13 -0300

    departamento repository

---
+++ app/app/src/main/java/com/ricky/desbravatask/data/repositoryImpl/DepartamentoRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.ricky.desbravatask.data.repositoryImpl
+
+import com.ricky.desbravatask.data.network.api.DepartamentoAPI
+import com.ricky.desbravatask.domain.models.Departamento
+import com.ricky.desbravatask.domain.repository.DepartamentoRepository
+import retrofit2.Response
+import javax.inject.Inject
+
+class DepartamentoRepositoryImpl @Inject constructor(
+    private val api: DepartamentoAPI
+) : DepartamentoRepository {
+    override suspend fun save(model: Departamento): Response<Departamento> {
+        return api.save(model)
+    }
+
+    override suspend fun getById(id: String): Response<Departamento> {
+        return api.getById(id)
+    }
+
+    override suspend fun update(model: Departamento): Response<Departamento> {
+        return api.update(model)
+    }
+
+    override suspend fun delete(id: String): Response<Void> {
+        return api.deleteById(id)
+    }
+
+}
---
+++ app/app/src/main/java/com/ricky/desbravatask/domain/repository/DepartamentoRepository.kt
@@ -0,0 +1,7 @@
+package com.ricky.desbravatask.domain.repository
+
+import com.ricky.desbravatask.domain.models.Departamento
+
+interface DepartamentoRepository : BaseRepository<Departamento, Departamento> {
+
+}
