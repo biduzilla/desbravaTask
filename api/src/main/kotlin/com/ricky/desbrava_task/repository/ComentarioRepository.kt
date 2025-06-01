515323b35d6d4cc710b2e37e9ad0266a8fe17e82
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-06-01 20:38:50 -0300

    comentario service

--- api/src/main/kotlin/com/ricky/desbrava_task/repository/ComentarioRepository.kt
+++ api/src/main/kotlin/com/ricky/desbrava_task/repository/ComentarioRepository.kt
@@ -1,6 +1,17 @@
 package com.ricky.desbrava_task.repository
 
 import com.ricky.desbrava_task.models.Comentario
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ComentarioRepository : BaseRepository<Comentario, String> {
+    @Query(
+        """
+        Select c from Comentario c 
+        where :search is null or c.nome like :comentario%
+    """
+    )
+    fun findAll(@Param("search") search: String?, pageable: Pageable): Page<Comentario>
 }
---
+++ api/src/main/kotlin/com/ricky/desbrava_task/service/impl/ComentarioServiceImpl.kt
@@ -0,0 +1,45 @@
+package com.ricky.desbrava_task.service.impl
+
+import com.ricky.desbrava_task.exceptions.DesbravaTaskErrorException
+import com.ricky.desbrava_task.models.Comentario
+import com.ricky.desbrava_task.repository.ComentarioRepository
+import com.ricky.desbrava_task.service.ComentarioService
+import com.ricky.desbrava_task.utils.I18n
+import com.ricky.desbrava_task.utils.getPageable
+import org.springframework.data.domain.Page
+import org.springframework.stereotype.Service
+
+@Service
+class ComentarioServiceImpl(
+    private val comentarioRepository: ComentarioRepository,
+    private val i18n: I18n
+) : ComentarioService {
+    override fun save(entity: Comentario): Comentario {
+        return comentarioRepository.save(entity)
+    }
+
+    override fun findById(id: String): Comentario? {
+        return comentarioRepository.findById(id)
+            .orElseThrow {
+                DesbravaTaskErrorException(i18n.getMessage("error.comentario.nao.encontrado"))
+            }
+    }
+
+    override fun findAll(search: String?, qtd: Int, page: Int): Page<Comentario> {
+        val pageable = getPageable(
+            page = page,
+            size = qtd
+        )
+
+        return comentarioRepository.findAll(search, pageable)
+    }
+
+    override fun findAll(): List<Comentario> {
+        return comentarioRepository.findAll()
+    }
+
+    override fun deleteById(id: String) {
+        comentarioRepository.deleteById(id)
+    }
+
+}
