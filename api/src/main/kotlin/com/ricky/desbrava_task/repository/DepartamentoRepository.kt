b6407c00b6ee6db9f59a34e3fe22ade4235c620c
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-06-01 20:18:50 -0300

    departamento service

--- api/src/main/kotlin/com/ricky/desbrava_task/repository/DepartamentoRepository.kt
+++ api/src/main/kotlin/com/ricky/desbrava_task/repository/DepartamentoRepository.kt
@@ -1,6 +1,18 @@
 package com.ricky.desbrava_task.repository
 
+import com.ricky.desbrava_task.models.Clube
 import com.ricky.desbrava_task.models.Departamento
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface DepartamentoRepository : BaseRepository<Departamento, String> {
+    @Query(
+        """
+        Select d from Departamento d 
+        where :search is null or d.nome like :search%
+    """
+    )
+    fun findAll(@Param("search") search: String?, pageable: Pageable): Page<Departamento>
 }
--- api/src/main/kotlin/com/ricky/desbrava_task/service/impl/DepartamentoServiceImpl.kt
+++ api/src/main/kotlin/com/ricky/desbrava_task/service/impl/DepartamentoServiceImpl.kt
@@ -1,4 +1,45 @@
 package com.ricky.desbrava_task.service.impl
 
-class DepartamentoServiceImpl {
+import com.ricky.desbrava_task.exceptions.DesbravaTaskErrorException
+import com.ricky.desbrava_task.models.Departamento
+import com.ricky.desbrava_task.repository.DepartamentoRepository
+import com.ricky.desbrava_task.service.DepartamentoService
+import com.ricky.desbrava_task.utils.I18n
+import com.ricky.desbrava_task.utils.getPageable
+import org.springframework.data.domain.Page
+import org.springframework.stereotype.Service
+
+@Service
+class DepartamentoServiceImpl(
+    private val departamentoRepository: DepartamentoRepository,
+    private val i18n: I18n
+) : DepartamentoService {
+    override fun save(entity: Departamento): Departamento {
+        return departamentoRepository.save(entity)
+    }
+
+    override fun findById(id: String): Departamento? {
+        return departamentoRepository.findById(id)
+            .orElseThrow {
+                DesbravaTaskErrorException(i18n.getMessage("error.departamento.nao.encontrado"))
+            }
+    }
+
+    override fun findAll(search: String?, qtd: Int, page: Int): Page<Departamento> {
+        val pageable = getPageable(
+            page = page,
+            size = qtd
+        )
+
+        return departamentoRepository.findAll(search, pageable)
+    }
+
+    override fun findAll(): List<Departamento> {
+        return departamentoRepository.findAll()
+    }
+
+    override fun deleteById(id: String) {
+        departamentoRepository.deleteById(id)
+    }
+
 }
